6bb82582c5e51b9c04bd7d59fdda7d6cff8d9f66
Looi Wei En <looi.weien02@student.usm.my>
2023-05-01 23:11:31 +0800

    Fix code to work, modification not complete

--- radix_sort.java
+++ radix_sort.java
@@ -33,14 +33,54 @@ public class radix_sort {
 
     public static void RadixSort(double[] arr) {
 
-        // Declaring 2 fixed-sized arrays (size 10) for 10 decimal places.
-        double[] array1 = new double[10];
-        double[] array2 = new double[10];
+        // Declaring 2 fixed-sized linked lists (size 10) for 10 decimal places.
+        LinkedList<Double>[] array1 = new LinkedList[10];
+        LinkedList<Double>[] array2 = new LinkedList[10];
+
+        int size = arr.length;
 
         int MaxIndex = GetMax(arr); // Get index value of maximum value in the array
         double MaxValue = arr[MaxIndex]; // Declare maximum value in the array
-        int num = GetDigits(MaxValue); // Get the number of digits in the maximum value
 
+        // Get the number of digits in the maximum value
+        // This is to determine the number of loops needed for the sort
+        int num = GetDigits(MaxValue);
+
+        // Creating buckets for each array index to store pointers
+        for (int i = 0; i < 10; i++) {
+            array1[i] = new LinkedList<Double>();
+            array2[i] = new LinkedList<Double>();
+        }
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < size; j++) {
+                int digit = (int) (arr[j] / Math.pow(10, i)) % 10;
+                array1[digit].add(arr[j]);
+            }
+            int x = 0, y = 0;
+            // write back to the array after each pass
+            while (x < 10) {
+                while (!array1[x].isEmpty()) {
+                    arr[y] = array1[x].remove();
+                    y++;
+                }
+                x++;
+            }
+        }
     }
 
+    // a utility function to print the sorted array
+    static void printArr(double[] arr) {
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        double[] arr = { 573, 25, 415, 12, 161, 6 };
+
+        // function call
+        RadixSort(arr);
+        printArr(arr);
+    }
 }
